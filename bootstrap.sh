54a5569db63d3c2b8ebea2a218dbf49cd3568023
Chris Wessels <undefined.za@gmail.com>
2014-06-06 15:29:00 +0100

    Correctly set hostname of devstack depolyment for host machine access.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,5 +2,7 @@
 set -e
 sudo apt-get install git -y
 git clone git://github.com/openstack-dev/devstack.git
-cd devstack; ./stack.sh
+cd devstack
+export SERVICE_HOST=devstack.vagrant.dev
+./stack.sh
 echo "Horizon is now accessible at http://devstack.vagrant.dev"
