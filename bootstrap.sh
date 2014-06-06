43535d48a24b4e5c20dfa59fb29c179217c6678f
Chris Wessels <undefined.za@gmail.com>
2014-06-06 15:22:50 +0100

    Correctly set hostname of devstack depolyment for host machine access.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,5 +5,6 @@ git clone git://github.com/openstack-dev/devstack.git
 cd devstack
 awk '/install_pip.sh/ { print; print "sudo pip install --upgrade setuptools"; next }1' stack.sh > stack_patched.sh
 chmod +x stack_patched.sh
+export SERVICE_HOST=devstack.vagrant.dev
 ./stack_patched.sh
 echo "Horizon is now accessible at http://devstack.vagrant.dev"
